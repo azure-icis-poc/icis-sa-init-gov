af3ed65cbb34220763b098e9d4f6be71a1245b25
송양종 (오픈채널사업TF) <yj.song@kt.com>
2023-11-06 11:25:56 +0000

    Update start.sh

--- resources/start.sh
+++ resources/start.sh
@@ -3,29 +3,30 @@
 
 
 # [1. JVM Option Set]----------------------------------------------------------------------#
-echo "JVM Option Set..."
-export JAVA_OPTS="-XX:+UseG1GC,-XX:+UnlockDiagnosticVMOptions,-XX:InitiatingHeapOccupancyPercent=35,-XX:G1ConcRefinementThreads=20,-Duser.language=ko,-Duser.country=KR"
-
+echo "1. JVM Option Set"
+#export JAVA_OPTS="-XX:+UseG1GC,-XX:+UnlockDiagnosticVMOptions,-XX:InitiatingHeapOccupancyPercent=35,-XX:G1ConcRefinementThreads=20,-Duser.language=ko,-Duser.country=KR"
+export JAVA_OPTS="-XX:+UnlockDiagnosticVMOptions,-XX:InitiatingHeapOccupancyPercent=35,-XX:G1ConcRefinementThreads=20,-Duser.language=ko,-Duser.country=KR"
 export JDK_JAVA_OPTIONS="${JAVA_OPTS}, ${JDK_JAVA_OPTIONS}"
 
 
 
 # [2. resources File Copy]----------------------------------------------------------------------#
-echo "JVM Option Set..."
+echo "2. resources File Copy..."
 cp -rf /app/resources/sa-gov/e2e-producer.properties /app/resources
 cp -rf /app/resources/sa-gov/e2e-collector.properties /app/resources
 
 
 
 # [3. 24by7 Mode]----------------------------------------------------------------------#
-echo "24by7 Mode Set..."
+echo "3. 24by7 Mode..."
 export MODE_24BY7=N
 
 
 # [4. App Copy]----------------------------------------------------------------------#
-echo "App Copy..."
+echo "4. App Copy..."
 cp -rf /app/*.jar /app/icis-app.jar
 
 
 # [5. App execution]----------------------------------------------------------------------#
+echo "5. App execution"
 java -jar /app/icis-app.jar
