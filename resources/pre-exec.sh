26bc6c3d1ec26b7361d776a92cb1a039cbf15de2
김선수 (KT협력사) <91340722@ktfriend.com>
2023-11-06 01:33:53 +0000

    Update pre-exec.sh

--- resources/pre-exec.sh
+++ resources/pre-exec.sh
@@ -1,4 +1,6 @@
 JAVA_OPTS=-Xms100m,-Xmx16G,-XX:MaxRAMFraction=1,-XX:+UseG1GC,-XX:+UnlockDiagnosticVMOptions,-XX:InitiatingHeapOccupancyPercent=35,-XX:G1ConcRefinementThreads=20,-Duser.language=ko,-Duser.country=KR
+export JAVA_OPTS
+
 JDK_JAVA_OPTIONS="${JAVA_OPTS}, ${JDK_JAVA_OPTIONS}"
 export JDK_JAVA_OPTIONS
 
