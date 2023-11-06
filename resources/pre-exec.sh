afcf0b19abde4cd1772b214863bce5b2208501f5
김선수 (KT협력사) <91340722@ktfriend.com>
2023-11-06 01:21:01 +0000

    Update pre-exec.sh

--- resources/pre-exec.sh
+++ resources/pre-exec.sh
@@ -1,5 +1,5 @@
-JAVA_OPTS="-Xms100m,-Xmx16G,-XX:MaxRAMFraction=1,-XX:+UseG1GC,-XX:+UnlockDiagnosticVMOptions,-XX:InitiatingHeapOccupancyPercent=35,-XX:G1ConcRefinementThreads=20,-Duser.language=ko,-Duser.country=KR"
-JDK_JAVA_OPTIONS="${JAVA_OPTS} ${JDK_JAVA_OPTIONS}"
+JAVA_OPTS=-Xms100m,-Xmx16G,-XX:MaxRAMFraction=1,-XX:+UseG1GC,-XX:+UnlockDiagnosticVMOptions,-XX:InitiatingHeapOccupancyPercent=35,-XX:G1ConcRefinementThreads=20,-Duser.language=ko,-Duser.country=KR
+JDK_JAVA_OPTIONS="${JAVA_OPTS}, ${JDK_JAVA_OPTIONS}"
 export JDK_JAVA_OPTIONS
 
 cp -rf /app/resources/sa-gov/e2e-producer.properties /app/resources
