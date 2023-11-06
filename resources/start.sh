70c00ff323d01484d13c7c893af5f62de70f42a2
송양종 (오픈채널사업TF) <yj.song@kt.com>
2023-11-06 10:46:38 +0000

    Add new file

---
+++ resources/start.sh
@@ -0,0 +1,11 @@
+JAVA_OPTS="-Xms100m,-Xmx16G,-XX:MaxRAMFraction=1,-XX:+UseG1GC,-XX:+UnlockDiagnosticVMOptions,-XX:InitiatingHeapOccupancyPercent=35,-XX:G1ConcRefinementThreads=20,-Duser.language=ko,-Duser.country=KR"
+export JAVA_OPTS
+
+JDK_JAVA_OPTIONS="${JAVA_OPTS}, ${JDK_JAVA_OPTIONS}"
+export JDK_JAVA_OPTIONS
+
+cp -rf /app/resources/sa-gov/e2e-producer.properties /app/resources
+cp -rf /app/resources/sa-gov/e2e-collector.properties /app/resources
+
+cp -rf /app/*.jar /app/icis-app.jar
+java -jar /app/icis-app.jar
