df4510fcfeeae6773d3f4e576ff381de70431066
김선수 (KT협력사) <91340722@ktfriend.com>
2023-11-08 07:32:02 +0000

    Update start.sh

--- resources/start.sh
+++ resources/start.sh
@@ -4,7 +4,7 @@
 # [1. JVM Option Set]----------------------------------------------------------------------#
 echo "1. JVM Option Set"
 export JAVA_OPTS="-Xms256m -Xmx16G -XX:+UseG1GC -XX:+UnlockDiagnosticVMOptions -XX:InitiatingHeapOccupancyPercent=35 -XX:G1ConcRefinementThreads=20 -Duser.language=ko, -Duser.country=KR"
-export JDK_JAVA_OPTIONS="${JAVA_OPTS}, ${JAVA_TOOL_OPTIONS}, ${JDK_JAVA_OPTIONS}"
+export JDK_JAVA_OPTIONS="${JAVA_OPTS} ${JAVA_TOOL_OPTIONS} ${JDK_JAVA_OPTIONS}"
 
 
 
