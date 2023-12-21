467f79b5af96f55e11cd0fd1fd78a22bf956eb7e
김선수 (ICIS Tr 아키텍처팀) <82279829@kt.com>
2023-12-21 07:01:32 +0000

    Update sa-init-gov-app.sh

--- resources/sa-init-gov-app.sh
+++ resources/sa-init-gov-app.sh
@@ -11,6 +11,6 @@ git clone https://82258624:Bz-rzP-3YZJFRzU4hNxk@gitlab.dspace.kt.co.kr/icis/sa/i
 echo "2.  Git Full & File Copy..."
 cd /icis-sa-init-gov 
 
-while true; do git pull; git checkout dev; sleep 1; cp -r /icis-sa-init-gov/resources/* /icis-sa-init-gov-app/dev/; git checkout sit; sleep 1; cp -r /icis-sa-init-gov/resources/* /icis-sa-init-gov-app/sit/; git checkout ait; sleep 1; cp -r /icis-sa-init-gov/resources/* /icis-sa-init-gov-app/ait/; git checkout rat; sleep 1; cp -r /icis-sa-init-gov/resources/* /icis-sa-init-gov-app/rat/; git checkout bat; sleep 1; cp -r /icis-sa-init-gov/resources/* /icis-sa-init-gov-app/bat/; git checkout test; sleep 1; cp -r /icis-sa-init-gov/resources/* /icis-sa-init-gov-app/test/; sleep 10;  done 
+while true; do git pull; git checkout dev; sleep 1; cp -r /icis-sa-init-gov/resources/* /icis-sa-init-gov-app/dev/; git checkout sit; sleep 1; cp -r /icis-sa-init-gov/resources/* /icis-sa-init-gov-app/sit/; git checkout ait; sleep 1; cp -r /icis-sa-init-gov/resources/* /icis-sa-init-gov-app/ait/; git checkout rat; sleep 1; cp -r /icis-sa-init-gov/resources/* /icis-sa-init-gov-app/rat/; git checkout bat; sleep 1; cp -r /icis-sa-init-gov/resources/* /icis-sa-init-gov-app/bat/; git checkout test; sleep 1; cp -r /icis-sa-init-gov/resources/* /icis-sa-init-gov-app/test; sleep 10;  done 
 
 # [3. test]----------------------------------------------------------------------#
