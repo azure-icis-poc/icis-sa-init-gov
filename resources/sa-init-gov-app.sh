b4e8801127959abc73de51acef86d16efe923a10
김선수 (ICIS Tr 아키텍처팀) <82279829@kt.com>
2023-12-22 01:57:57 +0000

    Update sa-init-gov-app.sh

--- resources/sa-init-gov-app.sh
+++ resources/sa-init-gov-app.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
 
-# [1. Git Clone]----------------------------------------------------------------------#
+# [1. Git Clone, Full % Copy]----------------------------------------------------------------------#
 echo "1. Git Clone"
 git clone https://82258624:Bz-rzP-3YZJFRzU4hNxk@gitlab.dspace.kt.co.kr/icis/sa/icis-sa-init-gov
+git clone https://82258624:Bz-rzP-3YZJFRzU4hNxk@gitlab.dspace.kt.co.kr/icis/sa/icis-oder-init-gov
 
 
 
@@ -11,6 +12,6 @@ git clone https://82258624:Bz-rzP-3YZJFRzU4hNxk@gitlab.dspace.kt.co.kr/icis/sa/i
 echo "2.  Git Full & File Copy..."
 cd /icis-sa-init-gov 
 
-while true; do git pull; git checkout dev; sleep 1; cp -r /icis-sa-init-gov/resources/* /icis-sa-init-gov-app/dev/; git checkout sit; sleep 1; cp -r /icis-sa-init-gov/resources/* /icis-sa-init-gov-app/sit/; git checkout ait; sleep 1; cp -r /icis-sa-init-gov/resources/* /icis-sa-init-gov-app/ait/; git checkout rat; sleep 1; cp -r /icis-sa-init-gov/resources/* /icis-sa-init-gov-app/rat/; git checkout bat; sleep 1; cp -r /icis-sa-init-gov/resources/* /icis-sa-init-gov-app/bat/; git checkout test; sleep 1; cp -r /icis-sa-init-gov/resources/* /icis-sa-init-gov-app/test/; sleep 10;  done 
+while true; do cd /icis-sa-init-gov; git pull; git checkout dev; sleep 1; cp -r /icis-sa-init-gov/resources/* /icis-sa-init-gov-app/dev/; git checkout sit; sleep 1; cp -r /icis-sa-init-gov/resources/* /icis-sa-init-gov-app/sit/; git checkout ait; sleep 1; cp -r /icis-sa-init-gov/resources/* /icis-sa-init-gov-app/ait/; git checkout rat; sleep 1; cp -r /icis-sa-init-gov/resources/* /icis-sa-init-gov-app/rat/; git checkout bat; sleep 1; cp -r /icis-sa-init-gov/resources/* /icis-sa-init-gov-app/bat/; cd /icis-oder-init-gov; git pull; sleep 2; cp -r /icis-oder-init-gov/test/* /icis-sa-init-gov-app/test/; sleep 10;  done 
 
 # [3. test]----------------------------------------------------------------------#
