2bcd89d1032224af651b2caa762a155acbf9da39
김선수 (ICIS Tr 아키텍처팀) <82279829@kt.com>
2023-12-22 06:05:43 +0000

    Update sa-init-gov-app.sh

--- resources/sa-init-gov-app.sh
+++ resources/sa-init-gov-app.sh
@@ -8,12 +8,17 @@ git clone https://82258624:Bz-rzP-3YZJFRzU4hNxk@gitlab.dspace.kt.co.kr/icis/sa/i
 echo "oder Git Clone"
 git clone https://82258624:Bz-rzP-3YZJFRzU4hNxk@gitlab.dspace.kt.co.kr/icis/oder/icis-oder-init-gov
 
+echo "bill Git Clone"
+git clone https://82258624:Bz-rzP-3YZJFRzU4hNxk@gitlab.dspace.kt.co.kr/icis/bill/icis-bill-init-gov
+
+echo "rater Git Clone"
+git clone https://82258624:Bz-rzP-3YZJFRzU4hNxk@gitlab.dspace.kt.co.kr/icis/rater/icis-rater-init-gov
 
 
 # [2. Git Full % Copy]----------------------------------------------------------------------#
 echo "2.  Git Full & File Copy..."
 cd /icis-sa-init-gov 
 
-while true; do cd /icis-sa-init-gov; git pull; git checkout dev; sleep 1; cp -r /icis-sa-init-gov/resources/* /icis-sa-init-gov-sa/dev/; git checkout sit; sleep 1; cp -r /icis-sa-init-gov/resources/* /icis-sa-init-gov-app/sit/; git checkout ait; sleep 1; cp -r /icis-sa-init-gov/resources/* /icis-sa-init-gov-sa/ait/; git checkout rat; sleep 1; cp -r /icis-sa-init-gov/resources/* /icis-sa-init-gov-sa/rat/; git checkout bat; sleep 1; cp -r /icis-sa-init-gov/resources/* /icis-sa-init-gov-sa/bat/; cd /icis-oder-init-gov; git pull; git checkout main; sleep 2;  cp -r /icis-oder-init-gov/* /icis-sa-init-gov-oder/ ; sleep 1;  done 
+while true; do cd /icis-sa-init-gov; git pull; git checkout dev; sleep 1; cp -r /icis-sa-init-gov/resources/* /icis-sa-init-gov-sa/dev/; git checkout sit; sleep 1; cp -r /icis-sa-init-gov/resources/* /icis-sa-init-gov-app/sit/; git checkout ait; sleep 1; cp -r /icis-sa-init-gov/resources/* /icis-sa-init-gov-sa/ait/; git checkout rat; sleep 1; cp -r /icis-sa-init-gov/resources/* /icis-sa-init-gov-sa/rat/; git checkout bat; sleep 1; cp -r /icis-sa-init-gov/resources/* /icis-sa-init-gov-sa/bat/; cd /icis-oder-init-gov; git pull; sleep 2; git checkout main; sleep 1;  cp -r /icis-oder-init-gov/* /icis-sa-init-gov-oder/ ; sleep 1; cd /icis-bill-init-gov; git pull; sleep 2; git checkout main; sleep 1;  cp -r /icis-bill-init-gov/* /icis-sa-init-gov-bill/ ; sleep 1; cd /icis-rater-init-gov; git pull; sleep 2; git checkout main; sleep 1;  cp -r /icis-rater-init-gov/* /icis-sa-init-gov-rater/ ; sleep 1; done 
 
 # [3. test]----------------------------------------------------------------------#
