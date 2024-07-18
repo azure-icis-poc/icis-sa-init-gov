863fd89860e00ddd1761b25226786a9d37c690a0
82258624@kt.com <82258624@kt.com>
2024-07-18 13:57:17 +0900

    gw 추가

---
+++ resources/init-start-gw.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+
+# [1. resources File Copy]----------------------------------------------------------------------#
+echo "1. resources File Copy..."
+cp -r /sa-gov-resource/e2e-producer.properties /sa-gov
+cp -r /sa-gov-resource/e2e-collector.properties /sa-gov
+cp -r /sa-gov-resource/truststore.jks /sa-gov
+cp -r /sa-gov-resource/start-gw.sh /sa-gov
+
---
+++ resources/start-gw.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+
+# [1. JVM Option Set]----------------------------------------------------------------------#
+echo "1. JVM Option Set"
+export JAVA_OPTS="-Duser.language=ko, -Duser.country=KR"
+export JDK_JAVA_OPTIONS="${JAVA_OPTS} ${JAVA_TOOL_OPTIONS} ${JDK_JAVA_OPTIONS} ${JAVA_OPTS_AGENT}"
+
+
+
+# [2. resources File Copy]----------------------------------------------------------------------#
+echo "2. resources File Copy..."
+cp -rf /app/resources/sa-gov/e2e-producer.properties /app/resources
+cp -rf /app/resources/sa-gov/e2e-collector.properties /app/resources
+cp -rf /app/resources/sa-gov/truststore.jks /app/resources
+
+
+
+# [3. 24by7 Mode]----------------------------------------------------------------------#
+echo "3. 24by7 Mode..."
+export MODE_24BY7=N
+
+
+
+# [4. App execution]----------------------------------------------------------------------#
+echo "4. App execution..."
+echo "app main class {$APP_MAIN_CLASS}"
+#java -cp /app/resources:/app/classes:/app/libs/* com.kt.icis.PPonApplication
+#java -cp /app/resources:/app/classes:/app/libs/* com.kt.icis.BackendApplication
+#java -cp /app/resources:/app/classes:/app/libs/* com.kt.icistr.rater.server.accounting.AccountingServer
+java -cp /app/resources:/app/classes:/app/libs/* $APP_MAIN_CLASS
