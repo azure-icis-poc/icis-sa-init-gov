7454ce68b47f0b8f7cfcd23357b00e9a5fcea793
LEE SEOK HWAN (이석환) <41466732+shclub@users.noreply.github.com>
2024-07-30 17:23:16 +0900

    Update init-start.sh

--- resources/init-start.sh
+++ resources/init-start.sh
@@ -3,8 +3,7 @@
 
 # [1. resources File Copy]----------------------------------------------------------------------#
 echo "1. resources File Copy..."
-cp -r /sa-gov-resource/e2e-producer.properties /sa-gov
-cp -r /sa-gov-resource/e2e-collector.properties /sa-gov
-cp -r /sa-gov-resource/truststore.jks /sa-gov
-cp -r /sa-gov-resource/start.sh /sa-gov
-
+cp -r /gov/resources/e2e-producer.properties /sa-gov
+cp -r /gov/resources/e2e-collector.properties /sa-gov
+cp -r /gov/resources/truststore.jks /sa-gov
+cp -r /gov/resources/start.sh /sa-gov
